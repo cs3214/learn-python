265c3715a8600267cd097c288d0b7e76023a8f06
cs3214 <0jj5d2073292w5p@au.com>
2024-10-22 15:10:20 +0900

    studying syntax

--- test.py
+++ test.py
@@ -1,1 +1,29 @@
 print('Hello World')
+print("Good morning")
+
+num1 = 1
+num2 = 20
+num3 = 20
+
+string_a = "アゴツマガツチ"
+
+bool_a = (num1 > num2)
+
+#リスト
+list_a = ["ララ", "クラッシュ", "アゴツ", "マガツチ", "参上"]
+
+print("変数の中身は" + str(num1))
+print("変数の中身は" + string_a)
+print("変数の中身は" + str(bool_a))
+print("変数の中身は" + str(list_a[num1]))
+
+print(num1 > num2 and num2 >= num3)
+print(num1 > num2 or num2 >= num3)
+
+for i in range(10):
+  if i == len(list_a) : break
+  elif i == 2: continue
+  print(list_a[i])
+
+
+
